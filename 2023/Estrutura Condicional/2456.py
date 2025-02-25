7c23cf4c397d0f1b29d6e322c0028e8ad8b07d08
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-02-25 20:48:40 -0300

    Create 2456.py

---
+++ 2023/Estrutura Condicional/2456.py
@@ -0,0 +1,8 @@
+a,b,c,d,e = map(int,input().split())
+lista2 = [a,b,c,d,e]
+if lista2 == sorted(lista2):
+    print("C")
+elif lista2 ==ed(l sortista2, reverse=True):
+    print("D")
+else:
+    print("N")
