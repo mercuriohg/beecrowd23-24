6831e0e6e4a50bc6a847df883e7eaabc8dc69c5a
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-02-26 20:31:04 -0300

    Update 1113.py

--- 2023/Repetição Indeterminada/1113.py
+++ 2023/Repetição Indeterminada/1113.py
@@ -1,1 +1,9 @@
-
+a,b = 0,1
+while a != b:
+    a,b = map(int,input().split())
+    if a > b:
+        print("Decrescente")
+    elif a == b and b == a:
+        break;
+    else:
+        print("Crescente")
