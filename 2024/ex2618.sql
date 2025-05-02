c3e17ebee72600687736e0df58321713faeb4cea
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-05-02 00:14:51 -0300

    Delete 2024/ex2618.sql

--- 2024/ex2618.sql
+++
@@ -1,4 +0,0 @@
-SELECT products.name AS product_name, providers.name AS provider_name, categories.name AS category_name
-FROM products
-JOIN providers ON products.id_providers = providers.id
-JOIN categories ON products.id_categories = categories.id
